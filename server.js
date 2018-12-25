794c86fdb250834c678f175862afd3c7457df560
mohamad arbash <mohamad.arbash@gmail.com>
2018-12-25 22:03:38 +0100

    server

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log  = req.method + req.url
+	console.log(now);
+	console.log(log);
+	fs.appendFile('server.log', log + '\r\n', (err) => {
+     if(err) {
+      console.log(err);
+	 }	 
+})
+	next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.send('<h1>hello express</h1>');
+});
+app.get('/json', (req, res) => {
+	res.send({
+		name: 'Mohamad',
+		likes: ['memo']
+	});
+});
+app.get('/home', (req, res) => {
+	res.render('home.hbs', {
+		welcomeTitle: ' hi yooo',
+		current: new Date().getFullYear()
+	});
+});
+
+app.get('/about', (req, res) => {
+res.render('about.hbs', {
+pageTitle: 'About Page',
+current: new Date().getFullYear()
+});
+}); 
+
+app.listen(3000, () => {console.log('hello ')});
